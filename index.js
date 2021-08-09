380a54615b774376663a66afe5628b61b0cd824e
Dennis1-1995 <83753035+Dennis1-1995@users.noreply.github.com>
2021-08-09 12:37:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('Hello Winc Academy');
+        
+        let firstName= 'Dennis';
+        console.log(firstName);
+        // If kan niet omdat deze gereserveerd is.
+        // 2 nr's 4+4 kan je optellen
+        // 2 strings kan je niet optellen
+
+        //Simpele sommetjes
+        console.log(3+5);
+        console.log(10-6);
+        console.log(2*8);
+        console.log(15/3);
+        console.log(10%3);
+
+        let leeftijd= 46;
+        console.log(typeof leeftijd);
+        leeftijd='46';
+        console.log(typeof leeftijd);
